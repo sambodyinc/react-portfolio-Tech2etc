f43b813b71dd80ad560c89df7a283032b6d2002c
sambodyinc <sambodyinc@gmail.com>
2024-02-24 17:42:54 +0300

    commit change p for contact.js

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -12,7 +12,7 @@ const Contact = () => {
       <Heroimg2
         imagebg="https://images.unsplash.com/photo-1587560699334-bea93391dcef?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         heading="CONTACT"
-        text="Let's have a chat."
+        text="Write me, let's have a chat."
       />
       <Form/>
       <Footer />
